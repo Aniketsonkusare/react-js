ff87772ba96101388c07577a1bc442054714bfaa
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-08-09 11:35:35 +0530

    props and proptype

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+// let name = "aniket";
+
+function App() {
+  return (
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
+    <Navbar title="TextUtils" />
+    {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
