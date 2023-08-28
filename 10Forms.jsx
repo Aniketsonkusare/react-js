4494ddf1f02a5c4d327809e2d66b15df6af66efd
Aniketsonkusare <aniketsonkusare70@gmail.com>
2023-08-28 20:55:17 +0530

    Forms

---
+++ 10Forms.jsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from 'react'
+
+function Forms() {
+    const inpref = useRef('')
+    const[data,setdata] = useState('')
+    const BtnClck = () =>{
+        console.log("submitted");
+        console.log(inpref.current.value);
+        if (inpref.current.value == "") {
+            alert("please enter the value 1")
+            inpref.current.focus();
+        }
+    }
+
+    // const handlechange = ()=>{
+    //     if (data == "") {
+    //         alert("please enter the value")
+            
+    //     }
+    // }
+  return (
+    <>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <form onSubmit={BtnClck} action='#'>
+                    <label>Name:</label>
+                    <input type="text" name='name' id='' className='form-control' ref={inpref}/>
+                    {/* <input type="text" name='name' className='form-control' value={data} onChange={(e) => {e.target.value(setdata)}} /> */}
+                    <button type='submit' className='btn btn-primary mt-2' name='submit'>submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Forms
